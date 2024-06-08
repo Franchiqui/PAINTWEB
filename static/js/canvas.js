f1939d6720c9f40d2f47e897094b8255a9960536
FranchiquiJ <167950323+FranchiquiJ@users.noreply.github.com>
2024-06-08 11:58:06 +0200

    f 	new file:   .dockerignore 	new file:   Dockerfile 	new file:   fly.toml 	new file:   static/css/style.css 	modified:   static/js/canvas.js 	modified:   templates/index.html

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById('drawingCanvas');
 const ctx = canvas.getContext('2d');
 let drawing = false;
+let brushSize = 2;
+let selectedColor = 'black';
 
 canvas.addEventListener('mousedown', (e) => {
     drawing = true;
@@ -14,16 +16,36 @@ canvas.addEventListener('mouseup', () => {
 
 canvas.addEventListener('mousemove', draw);
 
+document.getElementById('brushSize').addEventListener('input', (e) => {
+    brushSize = e.target.value;
+});
+
+document.getElementById('brushColor').addEventListener('input', (e) => {
+    selectedColor = e.target.value;
+});
+
 function draw(e) {
     if (!drawing) return;
-    ctx.lineWidth = 5;
+    ctx.lineWidth = brushSize;
     ctx.lineCap = 'round';
-    ctx.strokeStyle = 'black';
+    ctx.strokeStyle = selectedColor;
 
     ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
     ctx.stroke();
     ctx.beginPath();
     ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+}
+
+function selectBrush() {
+    selectedColor = document.getElementById('brushColor').value;
+}
+
+function selectEraser() {
+    selectedColor = '#FFFFFF';
+}
+
+function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 function saveDrawing() {
